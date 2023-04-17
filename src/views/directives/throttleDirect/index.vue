a9b301c2610b101e0d7ad92502c7da7c0cc57e53
wff <283953455@qq.com>
2023-04-17 15:00:18 +0800

    feat: 🚀 添加节流指令

--- src/views/directives/throttleDirect/index.vue
+++ src/views/directives/throttleDirect/index.vue
@@ -1,7 +1,12 @@
 <template>
-	<div class="card content-box">throttleDirect</div>
+	<div class="card content-box">
+		<span class="text">节流指令 🍇🍇🍇🍓🍓🍓</span>
+		<el-button type="primary" v-throttle="throttleClick">节流按钮 (每隔1S秒后执行)</el-button>
+	</div>
 </template>
 
-<script setup lang="ts" name="throttleDirect"></script>
-
-<style scoped lang="scss"></style>
+<script setup lang="ts" name="throttleDirect">
+const throttleClick = () => {
+	ElMessage.success('我是节流按钮触发的事件 🍍🍓🍌')
+}
+</script>
