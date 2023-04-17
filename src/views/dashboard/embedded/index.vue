e92915ffef1a688905f048eea824ead797202e89
wff <283953455@qq.com>
2023-04-17 16:49:05 +0800

    feat: 🚀 内嵌页面

--- src/views/dashboard/embedded/index.vue
+++ src/views/dashboard/embedded/index.vue
@@ -1,7 +1,11 @@
 <template>
-	<div class="card content-box">embedded</div>
+	<div class="card content-box">
+		<iframe src="https://cn.bing.com/" frameborder="0" class="full-iframe"></iframe>
+	</div>
 </template>
 
 <script setup lang="ts" name="embedded"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import './index.scss';
+</style>
