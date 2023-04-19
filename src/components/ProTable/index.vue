7a2960f6ce20f0fb983eaa3304d98f86b907c988
wff <283953455@qq.com>
2023-04-19 16:34:43 +0800

    feat(useProTable): 🚀 表格头部右边功能按钮

--- src/components/ProTable/index.vue
+++ src/components/ProTable/index.vue
@@ -6,8 +6,23 @@
 			<div class="header-button-lf">
 				<slot name="tableHeader" />
 			</div>
+			<div class="header-button-ri">
+				<slot name="toolButton">
+					<el-button :icon="Refresh" circle @click="getTableList" />
+					<el-button :icon="Printer" circle @click="handlePrint" />
+					<el-button :icon="Operation" circle @click="openColSetting" />
+					<el-button :icon="Search" circle @click="isShowSearch" />
+				</slot>
+			</div>
 		</div>
 	</div>
 </template>
 
-<script setup lang="ts" name="ProTable"></script>
+<script setup lang="ts" name="ProTable">
+import { Refresh, Printer, Operation, Search } from '@element-plus/icons-vue'
+
+const getTableList = () => {}
+const handlePrint = () => {}
+const openColSetting = () => {}
+const isShowSearch = () => {}
+</script>
